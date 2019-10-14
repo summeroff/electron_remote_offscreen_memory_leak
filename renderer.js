662af302e098b76e7e11eca4349f965496349da8
Vladimir Sumarov <summeroff@gmail.com>
2019-10-13 18:45:00 -0700

    leak on offscreen

--- renderer.js
+++ renderer.js
@@ -4,3 +4,27 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+
+const remote = require('electron').remote;
+const { BrowserWindow } = remote;
+
+let win_offscreen1;
+  
+win_offscreen1 = new BrowserWindow({   
+    show: false,
+    frame: false,
+    width: 600,
+    height: 600,
+    x: 100,
+    y: 100,
+    skipTaskbar: true,
+    thickFrame: false,
+    resizable: false,
+    webPreferences: { nodeIntegration: false, contextIsolation: true, offscreen: true }
+  })
+
+  win_offscreen1.loadURL('https://www.online-stopwatch.com/large-online-clock/')
+  win_offscreen1.webContents.on('paint', (event, dirty, image) => { })
+  win_offscreen1.webContents.setFrameRate(60);
+  win_offscreen1.webContents.invalidate();
+ 
